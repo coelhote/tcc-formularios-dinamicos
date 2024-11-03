31d133daa370cd845be756b1a471ebae00bd6273
coelhote <leonardocoelho@ymail.com>
2024-11-03 00:04:03 -0300

    fix: typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
     <header class="bg-primary text-white">
         <nav class="navbar navbar-expand-lg navbar-light container">
-            <a class="navbar-brand" href="{{ route('home') }}">Minha Aplicação</a>
+            <a class="navbar-brand" href="{{ route('home') }}"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
